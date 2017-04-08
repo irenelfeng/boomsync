ffb20176abddbaef3c3cf86ee45c3e915e439611
Irene <irene.l.feng.17@dartmouth.edu>
2017-04-08 14:15:37 -0400

    added level desc

--- src/App.js
+++ src/App.js
@@ -3,29 +3,31 @@ import './App.css'
 import Play from './Play'
 import Preview from './Preview'
 import CodeEditor from './CodeEditor'
+import levels from './levels/levels.js'
 
 class App extends Component {
   state = {
-    playing: false
+    playing: false,
+    level: 1
   }
 
-  play = () => this.setState({playing: true})
+  play = () => this.setState({ playing: true })
 
   render() {
     const { playing, level, code } = this.state
+    const description = '<p>'+levels[level].instructions.join('</p><p>')+'</p>'
 
     return (
       <div className="App">
         <div className="Left-sidebar">
-          <p className="Game-description">
-            Welcome to <code>boomsync</code>!
-          </p>
+          <div className="Game-description" dangerouslySetInnerHTML={{ __html: description }}>
+          </div>
           <CodeEditor />
         </div>
         <div className="Right-sidebar">
           {playing
             ? <Play {...{level, code}} />
-            : <Preview {...{level}} />
+            : <Preview {...{level, code}} />
           }
         </div>
       </div>

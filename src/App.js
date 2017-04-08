51adcd097e60d9491fb07db4ae8af7a4b7240e40
Byrne Hollander <bhollander823@gmail.com>
2017-04-08 11:57:16 -0400

    added CodeEditor component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CodeEditor from './CodeEditor';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: false
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="Game-description">
+          Welcome to <code>boomsync</code>!
         </p>
+        <CodeEditor />
       </div>
     );
   }

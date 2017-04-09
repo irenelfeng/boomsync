38e08cf4ccfd9cd9df170d27028df61332ba082d
Irene <irene.l.feng.17@dartmouth.edu>
2017-04-08 21:45:23 -0400

    added person to preview

--- src/Preview.js
+++ src/Preview.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import './Play.css'
 
-export default class Play extends Component {
+export default class Preview extends Component {
+
   render () {
     return (
-      <div className='play'>
+      <div className='preview'>
+        <img src={'/back.svg'} style={{
+            height: 100,
+            width: 100
+          }} ></img>
       </div>
     )
   }
